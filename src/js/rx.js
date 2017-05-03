09d0b70748307ee078d36f3eff98d35b3aa02986
t.shibuta <make.app.ts@gmail.com>
2017-05-03 16:38:37 +0900

    色々試してみる

--- src/js/rx.js
+++ src/js/rx.js
@@ -1,8 +1,31 @@
-import Rx from 'rx'
+import * as Rx from 'rxjs-es'
 
-const clickStream = Rx.Observable.fromEvent(document, 'mouseup')
-clickStream
-    .buffer(clickStream.throttle(250))
-    .map(function(x) {return x.length})
-    .filter(function(n) {return n >= 2})
-    .subscribe(function(n) {console.info(n + 'click')})
+const myObservable = new Rx.Subject()
+myObservable.subscribe((value) => {
+  console.log(`Sub: ${value}`)
+})
+myObservable.map(value => {
+  return value.trim()
+}).subscribe((value) => {
+  console.log(`Sub: ${value}`)
+})
+myObservable.next('  subject   ')
+console.log(myObservable)
+
+const observar = Rx.Observable.of('  observable   ')
+observar.subscribe((value) => {
+  console.log(`Ob: ${value}`)
+})
+
+observar.map((value) => {
+  return value.trim()
+})
+  .subscribe((value) => {
+    console.log(`Ob: ${value}`)
+  })
+
+// const myObservable2 = Rx.Observable.create(observer => {
+//   observer.next('foo 2')
+//   setTimeout(() => observer.next('bar'), 1000)
+// })
+// myObservable2.subscribe(value => console.log(value))
