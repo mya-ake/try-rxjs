5c7dd134734080fd4c079db88eaf84eb02ca8dab
t.shibuta <make.app.ts@gmail.com>
2017-05-06 15:28:30 +0900

    ;コロン排除

--- src/js/store/mutations.js
+++ src/js/store/mutations.js
@@ -1,6 +1,6 @@
 import TYPES from './mutation-types.js'
 
-const mutations = {};
+const mutations = {}
 
 mutations[TYPES.ADD_TASK] = (state, payload) => {
   state.tasks = state.tasks.concat(payload.task)
