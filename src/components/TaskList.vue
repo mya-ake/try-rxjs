7164c08a14f6e7a4d7bbb9216b1e6143f241eaaa
t.shibuta <make.app.ts@gmail.com>
2017-05-11 00:37:52 +0900

    タスク一覧のタイトル追加

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-list two-line subheader>
-    <v-list-item v-for="(task, index) in $store.state.tasks" v-bind:key="'task-' + index">
-      <v-list-tile avatar>
-        <v-list-tile-action>
-          <v-checkbox v-bind:input-value="task.done" v-on:change="updateDone(task, index)" success light />
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>{{task.title}}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list-item>
-  </v-list>
+  <section class="mt-4">
+    <h3 class="headline">Task list</h3>
+    <v-list two-line subheader>
+      <v-list-item v-for="(task, index) in $store.state.tasks" v-bind:key="'task-' + index">
+        <v-list-tile avatar>
+          <v-list-tile-action>
+            <v-checkbox v-bind:input-value="task.done" v-on:change="updateDone(task, index)" success light />
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{task.title}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-item>
+    </v-list>
+  </section>
 </template>
 
 <script>
